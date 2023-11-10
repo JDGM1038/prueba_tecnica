e04816ec8fdfe98a999cc6b331c87245c3d9da6a
jdgm <jdgm@gmail.com>
2023-11-09 23:50:23 -0500

    prueba tec

---
+++ ptecnica/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import MoviesPage from './pages/MoviesPage';
+/* import Foot from './components/Footer'; */
+/* import MoviesPage from './pages/MoviesPage'; */ 
+/* import HomePage from './pages/HomePage'; */
+
+
+
+
+function App() {
+  return (
+    <>
+      
+      {/* <HomePage/> */}
+      {/* <MoviesPage />  */}
+      {/* <Foot/> */}
+    <MoviesPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ ptecnica/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/foot.css'
+const Footer = () => {
+  return (
+    <div>
+      <h1>Pump it </h1>
+    </div>
+  )
+}
+
+export default Footer
