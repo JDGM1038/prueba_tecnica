29b6f95f1ac94dfbe770d7e095b17d49b7bed934
jdgm <jdgm@gmail.com>
2023-11-09 23:25:08 -0500

    terminado

---
+++ ptecnica/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import '../css/head.css'
+
+function Header() {
+  return (
+    <>
+    <nav>
+      <h1>DEMO Streaming</h1>
+      <ul>
+        <li><a href="/acerca">Log in</a></li>
+        <li><a href="/servicios">Servicios</a></li>
+      </ul>
+    </nav>
+    <div>Popular Titles</div>
+    </>
+  )
+}
+
+export default Header
